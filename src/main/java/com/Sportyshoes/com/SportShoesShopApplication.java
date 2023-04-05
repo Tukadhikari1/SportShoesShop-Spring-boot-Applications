d96df9be486c4c44502722c79f5912a89d7cc2cf
My get hub <tukadhikari07@gmail.com>
2023-04-05 15:46:07 +0300

    first commit

---
+++ src/main/java/com/Sportyshoes/com/SportShoesShopApplication.java
@@ -0,0 +1,13 @@
+package com.Sportyshoes.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportShoesShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportShoesShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Sportyshoes/com/SportShoesShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sportyshoes.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportShoesShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
